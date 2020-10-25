ed33bf144046b0ddc7e62c7524fc3d8d9ece4c48
giacomo draghi <draghi96@libero.it>
2020-10-25 16:59:23 +0100

    Created working framework. Next steps: 1-Adding searching ans Sorting 2-Styling the layout.

---
+++ MyScriptureJournal/MyScriptureJournal/Models/SeedData.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MyScriptureJournal.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MyScriptureJournalContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<MyScriptureJournalContext>>()))
+            {
+                // Look for any movies.
+                if (context.Scripture.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Scripture.AddRange(
+                    new Scripture
+                    {
+                        Book = "When Harry Met Sally",
+                        Chapter = "Romantic Comedy",
+                        Notes = "Romantic Comedy",
+                        InsertDate = DateTime.Parse("1989-2-12")
+                    },
+
+                    new Scripture
+                    {
+                        Book = "When Harry Met Sally",
+                        Chapter = "Romantic Comedy",
+                        Notes = "Romantic Comedy",
+                        InsertDate = DateTime.Parse("1989-2-12")
+                    },
+
+                    new Scripture
+                    {
+                        Book = "When Harry Met Sally",
+                        Chapter = "Romantic Comedy",
+                        Notes = "Romantic Comedy",
+                        InsertDate = DateTime.Parse("1989-2-12")
+                    },
+
+                    new Scripture
+                    {
+                        Book = "When Harry Met Sally",
+                        Chapter = "Romantic Comedy",
+                        Notes = "Romantic Comedy",
+                        InsertDate = DateTime.Parse("1989-2-12")
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
