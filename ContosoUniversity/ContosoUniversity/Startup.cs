d882249cddd74df77fa9ebdd62a9d1a0a7841fec
giacomo draghi <draghi96@libero.it>
2020-11-21 12:34:33 +0100

    Foreign key and navigation properties

--- ContosoUniversity/ContosoUniversity/Startup.cs
+++ ContosoUniversity/ContosoUniversity/Startup.cs
@@ -10,6 +10,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using ContosoUniversity.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http;
 
 namespace ContosoUniversity
 {
